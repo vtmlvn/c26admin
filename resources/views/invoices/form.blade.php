fbd31abaf72b8c1f8863c60e66f9a5967376bc5f
aulia_padli12 <aulia_padli12@apps.ipb.ac.id>
2018-12-12 18:58:46 +0700

    env gitignore

--- resources/views/invoices/form.blade.php
+++ resources/views/invoices/form.blade.php
@@ -6,32 +6,32 @@
             <p v-if="errors.invoice_no" class="error">@{{ errors.invoice_no[0] }}</p>
         </div>
         <div class="form-group">
-            <label>Client</label>
+            <label>Nama Customer</label>
             <input type="text" class="form-control" v-model="form.nama_customer">
             <p v-if="errors.nama_customer" class="error">@{{ errors.nama_customer[0]}}</p>
         </div>
     </div>
     <div class="col-sm-4">
         <div class="form-group">
-            <label>Client Address</label>
+            <label>Alamat Customer</label>
             <textarea class="form-control" v-model="form.alamat_customer"></textarea>
             <p v-if="errors.alamat_customer" class="error">@{{ errors.alamat_customer[0]}}</p>
         </div>
     </div>
     <div class="col-sm-4">
         <div class="form-group">
-            <label>Title</label>
+            <label>Judul</label>
             <input type="text" class="form-control" v-model="form.judul">
             <p v-if="errors.judul" class="error">@{{ errors.judul[0]}}</p>
         </div>
         <div class="row">
             <div class="form-group col-sm-6">
-                <label>Invoice Date</label>
+                <label>Tanggal Masuk</label>
                 <input type="date" class="form-control" v-model="form.tanggal_masuk">
                 <p v-if="errors.tanggal_masuk" class="error">@{{ errors.tanggal_masuk[0]}}</p>
             </div>
             <div class="form-group col-sm-6">
-                <label>Due Date</label>
+                <label>Tanggal Selesai</label>
                 <input type="date" class="form-control" v-model="form.tanggal_keluar">
                 <p v-if="errors.tanggal_keluar" class="error">@{{ errors.tanggal_keluar[0]}}</p>
             </div>
